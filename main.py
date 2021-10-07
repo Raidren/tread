03f0104e7874e0f1bbc6002bc8758415c845edc6
Raidren <90018704+Raidren@users.noreply.github.com>
2021-10-08 00:06:30 +0500

    Commit

--- main.py
+++ main.py
@@ -10,12 +10,11 @@ def Tread():
     t1 = Thread(writer())
     t1.start()
     t1.join()
-Tread()
-print((time.time() - start_time))
+Tread(),print((time.time() - start_time))
 def notTread():
     with open('test12.txt','w') as fout:
         for i in range (1000000):
             fout.write('1fsdfsdfsd')
-notTread()
-print((time.time() - start_time))
+notTread(),print((time.time() - (start_time)))
+
 #Выполнение через потоки выходят куда эффективнее, чем без них

957d2cd7852ce3a2f68102237424a36c5aecbfcc
Raidren <90018704+Raidren@users.noreply.github.com>
2021-10-08 01:19:35 +0500

    Commit

--- main.py
+++ main.py
@@ -2,23 +2,21 @@ from multiprocessing import Process
 from threading import Thread
 import time
 start_time = time.time()
-def writer():
-    with open("test12.txt", 'w') as fout:
-        for i in range(500000):
-            fout.write('1fjgkdfjgkdds')
-def Tread():
-    t1 = Thread(writer())
-    t1.start()
-    t1.join()
-Tread(),print((time.time() - start_time))
+# def writer():
+#     with open("test12.txt", 'w') as fout:
+#         for i in range(500000):
+#             fout.write('1fjgkdfjgkdds')
+# def tread():
+#     t1 = Thread(writer())
+#     t1.start()
+#     t1.join()
 def notTread():
     with open('test12.txt','w') as fout:
-        for i in range (1000000):
-            fout.write('1fsdfsdfsd')
-notTread(),print((time.time() - (start_time)))
-
-if __name__ == '__main__':
-    proc = Process(target=writer())
-    proc.start()
-print((time.time() - (start_time)))
-#Выполнение через потоки выходят куда эффективнее, чем без них
+        for i in range (500000):
+            fout.write('1fjgkdfjgkdds')
+    print((time.time() - (start_time)))
+# if __name__ == '__main__':
+#     proc = Process(target=tread())
+#     proc.start()
+#     print((time.time() - (start_time)))
+notTread()

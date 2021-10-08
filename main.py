3f414a93d81bdadd2b6f65a3636459a60ae0a254
Raidren <90018704+Raidren@users.noreply.github.com>
2021-10-08 13:31:12 +0500

    Commit

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
+import multiprocessing
 from multiprocessing import Process
-from threading import Thread
+
 import time
 start_time = time.time()
-# def writer():
-#     with open("test12.txt", 'w') as fout:
-#         for i in range(500000):
-#             fout.write('1fjgkdfjgkdds')
-# def tread():
-#     t1 = Thread(writer())
-#     t1.start()
-#     t1.join()
-def notTread():
-    with open('test12.txt','w') as fout:
-        for i in range (500000):
-            fout.write('1fjgkdfjgkdds')
-    print((time.time() - (start_time)))
-# if __name__ == '__main__':
-#     proc = Process(target=tread())
-#     proc.start()
-#     print((time.time() - (start_time)))
-notTread()
+f = open('text.txt', 'r')
+f1 = open('text1.txt', 'w')
+def reader_writer():
+            for i in f:
+                f1.write(i)
+if __name__ == '__main__':
+    multiprocessing.freeze_support()
+    for i in range(6):
+        multiprocessing.Process(target=reader_writer).start()
+    print('Время выполнения записи значений в файл: ',(time.time() - (start_time)))

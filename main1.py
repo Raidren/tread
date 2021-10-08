d251e1781ceac2b6bfd942223b0c742473b9574b
Raidren <90018704+Raidren@users.noreply.github.com>
2021-10-08 12:49:39 +0500

    Commit

---
+++ main1.py
@@ -0,0 +1,6 @@
+import time
+start_time = time.time()
+with open("text.txt",'w') as f:
+    for i in range (1000000):
+        f.write('wefted45fgf')
+print('Время выполнения записи значений в файл: ',(time.time() - (start_time)))
